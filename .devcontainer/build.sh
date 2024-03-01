390a893807cf8fa2d7a555e855a78917ea986fd7
Francisco Fernandes <46536211+Fyotta@users.noreply.github.com>
2024-03-01 13:30:11 -0300

    Update build.sh

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -2,19 +2,19 @@
 
 NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
 VERSION=$(python3 -c 'print(eval(open("package").read())["version"])')
-rm -f $NAME-$VERSION.mkp \
+rm -f ./dist/$NAME-$VERSION.mkp \
    /omd/sites/cmk/var/cat  check_mk/packages/${NAME}-*.mkp \
    /omd/sites/cmk/var/check_mk/packages_local/${NAME}-*.mkp ||:
 
 mkp -v package package 2>&1 | sed '/Installing$/Q' ||:
+mkdir -v ./dist
+cp /omd/sites/cmk/var/check_mk/packages_local/$NAME-$VERSION.mkp ./dist
 
-cp /omd/sites/cmk/var/check_mk/packages_local/$NAME-$VERSION.mkp .
-
-mkp inspect $NAME-$VERSION.mkp
+mkp inspect ./dist/$NAME-$VERSION.mkp
 
 # Set Outputs for GitHub Workflow steps
 if [ -n "$GITHUB_WORKSPACE" ]; then
-    echo "pkgfile=${NAME}-${VERSION}.mkp" >> $GITHUB_OUTPUT
+    echo "pkgfile=./dist/${NAME}-${VERSION}.mkp" >> $GITHUB_OUTPUT
     echo "pkgname=${NAME}" >> $GITHUB_OUTPUT
     echo "pkgversion=$VERSION" >> $GITHUB_OUTPUT
 fi
